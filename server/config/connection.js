b245514aaeb9104672e7e5056f6ef7ecc1476c76
Yu-ChengFan <42628546+Yu-ChengFan@users.noreply.github.com>
2022-02-15 19:35:17 -0700

    config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-
+const v = 0
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+
 });
 
 module.exports = mongoose.connection;
